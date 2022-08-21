499e58d7829997c121281d263d7ef410a4a47d36
mitchelldyer01@gmail.com <mitchelldyer01@gmail.com>
2022-08-20 21:47:34 -0700

    Get middleware properly loading

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const PUBLIC_FILE = /\.(.*)$/;
+  const path = request.nextUrl.pathname;
+
+  // Skip internals
+  if (
+    path.startsWith('/_next') ||
+    path.startsWith('/api') ||
+    path.startsWith('/static') ||
+    PUBLIC_FILE.test(path)
+  ) 
+    return NextResponse.next();
+
+  // Check auth
+  switch (path) {
+    case '/login':
+      return NextResponse.next();
+    default:
+      const url = request.nextUrl.clone();
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
+  }  
+}
